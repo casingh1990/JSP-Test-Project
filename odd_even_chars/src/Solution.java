a62d063144e04acca35e2ec00f1b193e63251129
casingh1990 <32516406+casingh1990@users.noreply.github.com>
2017-11-09 11:35:34 -0500

    OddEven chars of String
    Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2space-separated strings on a single line

--- odd_even_chars/src/Solution.java
+++ odd_even_chars/src/Solution.java
@@ -1,22 +1,43 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Solution {
-  
-    public Solution(){
-      
-    }
-    public void find_odd_even_substrings(String str){
-    }
-            
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        String[] cases = new String[n];
-        for (int i=0; i<n; i++){
-          cases[i] = scan.next();
+  public String cases[];
+
+  public Solution(String cases[]) {
+    this.cases = cases;
+  }
+
+  public void find_odd_even_substrings() {
+    for (String cur : this.cases) {
+      String odd = "";
+      String even = "";
+      char[] chars = cur.toCharArray();
+      
+      for (int i=0; i<chars.length; i++){
+        if ((i%2)==0){
+          even += Character.toString(chars[i]);
+        }else{
+          odd += Character.toString(chars[i]);
         }
+      }//end for char
+      
+      //printing the results
+      System.out.println(even + " " + odd);
+      
+    }//end foreach string
+  }
+
+  public static void main(String[] args) {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    Scanner scan = new Scanner(System.in);
+    int n = scan.nextInt();
+    String[] cases = new String[n];
+    for (int i = 0; i < n; i++) {
+      cases[i] = scan.next();
     }
+    new Solution(cases).find_odd_even_substrings();;
+  }
 }
